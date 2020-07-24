874e1aae1d07332171e2659d490402690383b218
Maksim Fomenok <maksim.fomenok@gmail.com>
2020-07-24 19:53:24 +0300

    process errors on fetching random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -3,12 +3,14 @@ import "./random-planet.scss";
 import SwapiService from "../../services/swapi-service";
 import Loader from "../common/loader";
 import PlanetView from "./planet-view";
+import ErrorIndicator from "../common/error-indicator";
 
 class RandomPlanet extends Component {
     swapiService = new SwapiService();
     state = {
         planet: {},
-        loaded: false
+        loaded: false,
+        error: false
     };
 
     componentDidMount() {
@@ -22,24 +24,36 @@ class RandomPlanet extends Component {
         });
     }
 
+    onError = () => {
+        this.setState({
+            error: true,
+            loaded: true
+        });
+    }
+
     updatePlanet = () => {
         this.swapiService
             .getPlanet(12)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
     }
 
     render() {
-        const { planet, loaded } = this.state;
+        const { planet, loaded, error } = this.state;
+        const hasData = loaded && !error;
+
         const loader = loaded ? null : <Loader />;
-        const content = loaded ? <PlanetView planet={planet} /> : null;
+        const errorMessage = error ? <ErrorIndicator /> : null;
+        const content = hasData ? <PlanetView planet={planet} /> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {loader}
                 {content}
             </div>
         );
     }
-};
+}
 
 export default RandomPlanet;
