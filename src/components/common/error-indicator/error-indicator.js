b43b0a7ab9429f88e4816fecc58755193ffb62ce
Maksim Fomenok <maksim.fomenok@gmail.com>
2020-07-24 19:51:45 +0300

    added error indicator component

---
+++ src/components/common/error-indicator/error-indicator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./error-indicator.scss";
+
+const ErrorIndicator = () => {
+    return (
+        <div className="error-indicator">
+            <span className="boom">BOOM!</span>
+            <span>something has gone terribly wrong</span>
+            <span>(but we already sent droids to fix it)</span>
+        </div>
+    );
+};
+
+export default ErrorIndicator;
