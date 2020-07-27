e62ff3e7292af3b4f17f85ca305e7114ddb4d577
Maksim Fomenok <maksim.fomenok@gmail.com>
2020-07-28 00:50:03 +0300

    added method for getting person image

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -5,6 +5,7 @@ import StarshipTransformer from "./data-transformers/starship-transformer";
 export default class SwapiService {
 
     _apiBase = 'https://swapi.dev/api/';
+    _imageBase = 'https://starwars-visualguide.com/assets/img';
 
     getResource = async(resource) => {
         const res = await fetch(`${this._apiBase}${resource}/`);
@@ -21,6 +22,10 @@ export default class SwapiService {
         const person = await this.getResource(`people/${id}`);
 
         return PersonTransformer.transform(person);
+    }
+
+    getPersonImage = (id) => {
+        return `${this._imageBase}/characters/${id}.jpg`
     }
 
     getAllPersons = async() => {
