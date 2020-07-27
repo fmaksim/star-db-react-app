9bc7fb7a4d81bbece29a6b0fa29ead437d3674ad
Maksim Fomenok <maksim.fomenok@gmail.com>
2020-07-28 00:49:15 +0300

    added error boundry to root app component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import "./app.scss";
 import Header from "../header";
 import RandomPlanet from "../random-planet/random-planet";
-import ErrorBoundry from "../common/error-boundry/error-boundry";
+import ErrorBoundry from "../common/error-boundry";
 import PersonPage from "../pages/persons";
 
 export default class App extends Component {
